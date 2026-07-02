021b373c8a1a306e5f0d5e9cae195858b5c3315a
Black Code <black.code@live.com>
2026-07-02 21:08:33 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -250,7 +250,7 @@ const ICON_LOGOUT= `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="1
 
 function sidebarHtml(active) {
   const nav = [
-    { id: "links",  label: "Dashboard", href: "/admin",        icon: ICON_LINKS },
+    { id: "links",  label: "Dashboard", href: "/admin",        icon: `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/><polyline points="9 22 9 12 15 12 15 22"/></svg>` },
     { id: "add",    label: "Add Link", href: "/admin/add",    icon: ICON_ADD   },
     { id: "stats",  label: "Stats",    href: "/admin/stats",  icon: ICON_STATS },
   ];
